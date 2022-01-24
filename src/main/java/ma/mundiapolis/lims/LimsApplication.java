506a705243f12dc5050061737c6a7afe9238591d
Ilyas <ilmakroum@gmail.com>
2022-01-24 22:45:05 +0100

    Create spring application using spring initializr

---
+++ src/main/java/ma/mundiapolis/lims/LimsApplication.java
@@ -0,0 +1,10 @@
+package ma.mundiapolis.lims;
+
+import org.springframework.boot.CommandLineRunner;
+@SpringBootApplication
+public class LimsApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimsApplication.class, args);
+    }
+}
