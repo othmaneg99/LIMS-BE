979daa88611014c8141db781ca020728d0853c3e
Ilyas <ilmakroum@gmail.com>
2022-01-31 00:23:47 +0100

    Setup authentication using Spring Security with JWT #2

--- src/main/java/ma/mundiapolis/lims/security/SecurityConfig.java
+++ src/main/java/ma/mundiapolis/lims/security/SecurityConfig.java
@@ -53,8 +53,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .authorizeRequests().antMatchers("/auth/**").permitAll()
+//                .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
