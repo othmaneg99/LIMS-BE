1431793456f156f10bdee0542bf576d7fdc3ed1a
Ilyas <ilmakroum@gmail.com>
2022-01-31 11:28:35 +0100

    Allow articles images to public temporarily

--- src/main/java/ma/mundiapolis/lims/security/SecurityConfig.java
+++ src/main/java/ma/mundiapolis/lims/security/SecurityConfig.java
@@ -53,8 +53,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/auth/**").permitAll()
-//                .antMatchers("/api/test/**").permitAll()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/photoArticle/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
